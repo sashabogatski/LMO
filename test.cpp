af02aab34ff4fda3e7de6b62dac822d8fe041d92
digitaldrug <the_naskha@mail.ru>
2019-10-14 22:35:16 +1000

    bug

--- test.cpp
+++ test.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
-using namespase std;
-int main
+#include <iostream> 
+#include <time.h> 
+using namespace std;
+int main()
 {
-	cout << "polychilos";
+	srand(time(0));
+	int i, z;
+	int a[4][4];
+	for (int i = 0; i < 4; i++)
+	{
+		for (int z = 0; z < 4; z++)
+		{
+			a[i][z] = rand() % 19 + 1;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		for (int z = 0; z < 4; z++)
+		{
+			cout « a[i][z]«"\t";
+		}
+		cout « "\n";
+	}
+	return 0;
 }
